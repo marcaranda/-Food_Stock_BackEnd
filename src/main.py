3dc2aad2bf300e8a03d589833ca2423929bc803d
marcaranda <marc.aranda@estudiantat.upc.edu>
2024-12-01 09:39:24 +0100

    cors

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.service.stock import router as stock_router
 from src.service.diet import router as diet_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Tu URL del frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos HTTP
+    allow_headers=["*"],  # Permite todos los encabezados
+)
 
 # Incluir rutas desde routes.py
 app.include_router(stock_router, tags=["Stock"])
